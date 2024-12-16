1038ecaef5b685b77a142cbacead7251f7c8b3f0
Park Yoonseo <patpark97@gmail.com>
2024-12-16 16:34:26 +0900

    feat: [박윤서] Footer UI 부분 Pagination 기능 구현 [RB-19]

--- src/components/common/searchBar/CommonSearchBar.tsx
+++ src/components/common/searchBar/CommonSearchBar.tsx
@@ -1,31 +1,27 @@
-import React, { useState  } from 'react'
-import styles from './CommontSearchBar.module.scss'
 import { useRecoilState } from 'recoil'
 import { searchState } from '@/recoil/atoms/searchState'
+import { pageState } from '@/recoil/atoms/pageState'
+import React, { useState  } from 'react'
+import styles from './CommontSearchBar.module.scss'
 
 function CommonSearchBar() {
   const [search, setSearch] = useRecoilState(searchState)
+  const [page, setPage] = useRecoilState(pageState)
   const [text, setText] = useState('')
   const onChange = (event) => {
     setText(event.target.value)
   }
   const onSearch = () => {
-    if (text === '') {
-      // input 태그 안에 빈 값으로 검색했을 때 => searching default value
-      setSearch('Korea')
-    } else {
-      setSearch(text) // 작성한 input value 값 할당
-    }
+    if (text === '') setSearch('Korea') // input 태그 안에 빈 값으로 검색했을 때 => searching default value
+    else setSearch(text) // 작성한 input value 값 할당
+    setPage(1)
   }
 
   const handleKeyDown = (event: React.KeyboardEvent) => {
     if (event.key === 'Enter') {
-      if (text === '') {
-        // input 태그 안에 빈 값으로 검색했을 때 => searching default value
-        setSearch('Korea')
-      } else {
-        setSearch(text) // 작성한 input value 값 할당
-      }
+      if (text === '') setSearch('Korea') // input 태그 안에 빈 값으로 검색했을 때 => searching default value
+      else setSearch(text) // 작성한 input value 값 할당
+      setPage(1)
     }
   }
   
