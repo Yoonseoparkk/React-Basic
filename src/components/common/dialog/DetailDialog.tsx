a91d262ff7b063b90af89e1942a1a1fac8738640
Park Yoonseo <patpark97@gmail.com>
2024-12-19 13:29:23 +0900

    feat: [박윤서] 북마크 기능 구현 [RB-21]

--- src/components/common/dialog/DetailDialog.tsx
+++ src/components/common/dialog/DetailDialog.tsx
@@ -1,5 +1,12 @@
 import { CardDTO, Tag } from "@/pages/index/types/card"
 import styles from "./DetailDialog.module.scss"
+import toast, {toastConfig} from 'react-simple-toasts'
+import 'react-simple-toasts/dist/theme/dark.css'
+import { useEffect, useState } from "react"
+
+toastConfig(
+    {theme:'dark'}
+)
 
 interface Props {
     data: CardDTO
@@ -7,9 +14,45 @@ interface Props {
 }
 
 function DetailDialog({data, handleDialog }: Props ) {
+    const [bookmark, setBookmark] = useState(false)
+
+    // 다이얼로그 끄기
     const closeDialog = () => {
          handleDialog(false)
     }
+
+    // 북마크 추가 이벤트
+    const addBookMark = (selected: CardDTO) => {
+        setBookmark(true)
+
+        const getLocalStorage = JSON.parse(localStorage.getItem('bookmark'))
+        // 1. 로컬스토리지에 bookmark 이라는 데이터가 없을 경우
+        if(!getLocalStorage || getLocalStorage === null) {
+            localStorage.setItem('bookmark', JSON.stringify([selected]))
+            toast("해당 이미지를 북마크에 저장하였습니다. 😊")
+        } else {
+            // 2. 해당 이미지가 이미 로컬스토리지 bookmark라는 데이터에 저장되어 있을 경우
+            if(getLocalStorage.findIndex((item: CardDTO) => item.id === selected.id) > -1) {
+                toast("해당 이미지는 이미 북마크에 추가된 상태입니다. ❌")
+            } else {
+                // 3. 해당 이미지가 로컬스토리지 bookmark라는 데이터에 저장되어 있지 않을 경우
+                // + bookmark라는 데이터에 이미 어떤 값이 담겨 있는 경우
+                const res = [...getLocalStorage]
+                res.push(selected)
+                localStorage.setItem('bookmark', JSON.stringify(res))
+
+                toast("해당 이미지를 북마크에 저장하였습니다. 😊")
+            }
+        }
+    }
+
+    useEffect(() => {
+        const getLocalStorage = JSON.parse(localStorage.getItem('bookmark'))
+
+        if (getLocalStorage && getLocalStorage.findIndex((item: CardDTO) => item.id === data.id) > -1) {
+            setBookmark(true)
+        } else if (!getLocalStorage) return
+    }, [])
 
     return (
         <div className={styles.container}>
@@ -26,11 +69,17 @@ function DetailDialog({data, handleDialog }: Props ) {
                         <span className={styles.close__authorName}>{data.user.name}</span>
                     </div>
                     <div className={styles.bookmark}>
-                        <button className={styles.bookmark__button}>
+                        <button className={styles.bookmark__button} onClick={() => addBookMark(data)}>
                             {/* 구글 아이콘을 사용 */}
-                            <span className="material-symbols-outlined" style={{ fontSize: 16 + 'px'}}>
-                                favorite 
-                            </span>
+                            {bookmark === false ? (
+                                <span className="material-symbols-outlined" style={{fontSize: 16 + 'px'}}>
+                                    favorite
+                                </span>
+                            ) : (
+                                <span className="material-symbols-outlined" style={{fontSize: 16 + 'px', color: 'red'}}>
+                                    favorite
+                                </span>
+                            )}
                             북마크
                         </button>
                         <button className={styles.bookmark__button}>다운로드</button>
